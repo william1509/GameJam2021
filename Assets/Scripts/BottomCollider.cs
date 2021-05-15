df64bf517807f418c79405bed4f32fa5eaeb9fe7
william <william_martineau@hotmail.com>
2021-05-14 20:21:58 -0400

    initial commit

---
+++ Assets/Scripts/BottomCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Changing world");
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("touching ground");
+        transform.parent.SendMessage("Testing");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rigidbody;
+    private float jumpSpeed = 20.0f;
+    private bool canJump = false;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Changing worlds");
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+            rigidbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            canJump = false;
+        } 
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            rigidbody.AddForce(new Vector2(-2, 0) * jumpSpeed);
+        } 
+        else if(Input.GetKeyDown(KeyCode.E)) {
+            //Debug.Log("Changing world");
+        }
+    }
+
+    public void Testing()
+    {
+        canJump = true;
+    }
+
+}

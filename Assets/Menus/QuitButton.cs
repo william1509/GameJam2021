5f8be7a8869ad47227fcb1b98990539f1f6e18d1
Carl <corteocarl@gmail.com>
2021-05-15 00:07:58 -0400

    Menus work

---
+++ Assets/Menus/QuitButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class QuitButton : MenuButton
+{
+    protected override void ClickAction()
+    {
+        Debug.Log("Quit game!");
+        Application.Quit();
+    }
+}

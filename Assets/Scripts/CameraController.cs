fded4713fcac796e0199382100af5834f90350eb
william <william_martineau@hotmail.com>
2021-05-15 12:15:40 -0400

    commit before merge

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform objectToFollow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(objectToFollow.position.x, objectToFollow.position.y, transform.position.z);
+    }
+}

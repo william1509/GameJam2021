e10577a60a30410ed9f804542e6164665f715866
Carl <corteocarl@gmail.com>
2021-05-16 12:00:02 -0400

    Menu select sound

--- Assets/Menus/MenuButton.cs
+++ Assets/Menus/MenuButton.cs
@@ -3,13 +3,25 @@ using UnityEngine.EventSystems;
 
 public abstract class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    static string buttonSelectedSound = "Sounds/ButtonSelect";
     public Animator menuAnimator;
     bool clicked = false;
 
+    AudioSource buttonSelectedSource;
+
+    void Start()
+    {
+        buttonSelectedSource = gameObject.AddComponent<AudioSource>();
+        buttonSelectedSource.clip = Resources.Load(buttonSelectedSound) as AudioClip;
+    }
+
     public void OnPointerEnter(PointerEventData pEvent)
     {
-        if(!clicked)
+        if (!clicked)
+        {
+            buttonSelectedSource.Play();
             GetComponent<Animator>().Play("Selected");
+        }
     }
 
     public void OnPointerExit(PointerEventData pEvent)
